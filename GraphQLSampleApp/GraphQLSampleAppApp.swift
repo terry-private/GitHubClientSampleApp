3256d2ea3c490f42563afc8ffc04c1d9d17ecbd1
terry <teruhito.wakae@chatwork.com>
2023-10-04 18:31:28 +0900

    Initial Commit

---
+++ GraphQLSampleApp/GraphQLSampleAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  GraphQLSampleAppApp.swift
+//  GraphQLSampleApp
+//
+//  Created by cw-teruhito.wakae on 2023/10/02.
+//
+
+import SwiftUI
+import AppFeature
+
+@main
+struct GraphQLSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
